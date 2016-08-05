1b4dbe4a6e77355f4f18e08e5105040a5dd7d70d
Aslela <vickysiswanto@gmail.com>
2016-08-05 16:43:01 +0700

    Article Link
    Article Link

--- application/views/article_section.php
+++ application/views/article_section.php
@@ -5,32 +5,38 @@
 
         <div id="article-shortcut-container">
             <div class="btn-group btn-group-justified" role="group" id="article-shortcut-wrapper">
-                <a href="#" class="btn btn-default active" role="button">ALL</a>
-                <a href="#" class="btn btn-default" role="button">POPULAR</a>
+                <a href="<?=site_url('fe/article/lastArticleList')?>" target="_blank" class="btn btn-default active" role="button">ALL</a>
+                <a href="<?=site_url('fe/article/popular')?>" target="_blank" class="btn btn-default" role="button">POPULAR</a>
             </div>
         </div>
 
         <div id="article-category-container" class="row">
             <div class="article-category-item col-md-4">
-                <div class="item-box hvr-glow">
-                    <h1 class="article-title" align="center">LIFESTYLE</h1>
-                    <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/lifestyle.png">
-                    <p class="article-desc">All about lifestyle</p>
-                </div>
+                <a href = "<?=site_url('fe/article/articleCategory/Lifestyle')?>" target="_blank">
+                    <div class="item-box hvr-glow">
+                        <h1 class="article-title" align="center">LIFESTYLE</h1>
+                        <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/lifestyle.png">
+                        <p class="article-desc">All about lifestyle</p>
+                    </div>
+                </a>
             </div>
             <div class="article-category-item col-md-4">
-                <div class="item-box hvr-glow">
-                    <h1 class="article-title" align="center">TUTORIAL</h1>
-                    <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/tutorial.png">
-                    <p class="article-desc">Get all the latest information about technology all around the world</p>
-                </div>
+                <a href = "<?=site_url('fe/article/articleCategory/Tutorial')?>" target="_blank">
+                    <div class="item-box hvr-glow">
+                        <h1 class="article-title" align="center">TUTORIAL</h1>
+                        <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/tutorial.png">
+                        <p class="article-desc">Get all the latest information about technology all around the world</p>
+                    </div>
+                </a>
             </div>
             <div class="article-category-item col-md-4">
-                <div class="item-box hvr-glow">
-                    <h1 class="article-title" align="center">TECHNOLOGY</h1>
-                    <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/technology.png">
-                    <p class="article-desc">Learn with us to improve your technology knowledge</p>
-                </div>
+                <a href = "<?=site_url('fe/article/articleCategory/Technology')?>" target="_blank">
+                    <div class="item-box hvr-glow">
+                        <h1 class="article-title" align="center">TECHNOLOGY</h1>
+                        <img class="img-responsive article-img" src="<?php echo base_url();?>assets/image/category/technology.png">
+                        <p class="article-desc">Learn with us to improve your technology knowledge</p>
+                    </div>
+                </a>
             </div>
         </div>
 
